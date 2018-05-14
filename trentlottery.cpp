4c7cb377ecf6a0278e24429df11f3ed4d18fbaa1
TrentLottery <trentlottery@protonmail.com>
2018-05-14 16:39:01 +0800

    modify

--- trentlottery.cpp
+++ trentlottery.cpp
@@ -1,22 +1,44 @@
 #include "trentlottery.hpp"
 
-using namespace eosio;
+using namespace std;
 
-void trentlottery::playerbet(uint64_t period, account_name player, const asset& bet, const uint32_t buyCnt, vector<uint16_t> &bills) {
+//@abi action
+void trentlottery::playerbet(uint64_t draw, account_name player, const asset& bet, const uint32_t buycnt, std::vector<uint16_t> bills) {
     eosio_assert( bet.symbol == S(4,EOS) , "only EOS token allowed" );
     eosio_assert( bet.is_valid(), "invalid bet" );
     eosio_assert( bet.amount > 0, "must bet positive quantity" );
     require_auth(player);
 
-    offer_bets.emplace(_self, [&](auto& offer_bet) {
-        offer_bet.id        = offer_bets.available_primary_key();
-        offer_bet.player    = player;
-        offer_bet.periods   = period;
-        offer_bet.bet       = bet;
-        offer_bet.buyCnt    = buyCnt;
-        offer_bet.buyLottos = bills;
-        offer_bet.buyTime   = now();
+    // action(
+    //     permission_level{player, N(active)},
+    //     N(eosio.token), N(transfer),
+    //     std::make_tuple(player, _self, bet, std::string(""))
+    // ).send();
+
+    eosio::print("begin to write db\n");
+
+    auto bets_itr = offerbets.emplace(_self, [&](auto &offer) {
+        eosio::print("new offer bet");
+        uint64_t id = offerbets.available_primary_key();
+        eosio::print("new available key:", id);
+        offer.id = id;
+        offer.player = player;
+        offer.draw = draw;
+        offer.bet = bet;
+        offer.buycnt = buycnt;
+        offer.buylottos = bills;
+        offer.buytime = now();
+    });
+    eosio::print("end write", bets_itr->id);
+}
+
+void trentlottery::creategame(uint64_t draw, const asset &jackpot, uint16_t status, std::vector<uint16_t> hitnum) {
+    games.emplace(_self, [&](auto &game) {
+        game.draw       = draw;
+        game.jackpot    = jackpot;
+        game.status     = status;
+        game.hitnum     = hitnum;
     });
 }
 
-EOSIO_ABI(trentlottery, (playerbet))
+EOSIO_ABI(trentlottery, (playerbet)(creategame))
--- trentlottery.hpp
+++ trentlottery.hpp
@@ -4,78 +4,90 @@
 #include <eosiolib/asset.hpp>
 #include <string>
 
-namespace eosio {
-    class trentlottery : public eosio::contract {
-        private:
-            //@abi table game i64
-            struct game {
-                uint64_t                period;         // 双色球期数
-                asset                   jackpot;        // 资金池总量
-                uint16_t                status;         // 此轮双色球游戏的状态，未开奖、正在开奖、已开奖
-                vector<uint16_t>        winning_number; // 当期开奖号码，红球、蓝球都放在一个数组中，前面6个表示红球，最后一个表示蓝球
+using eosio::action;
+using eosio::asset;
+using eosio::const_mem_fun;
+using eosio::indexed_by;
+using eosio::key256;
+using eosio::name;
+using eosio::permission_level;
+using eosio::print;
 
-                uint64_t        primary_key()const { return period; }
+class trentlottery : public eosio::contract
+{
+  private:
+    //@abi table games i64
+    struct game
+    {
+        uint64_t draw;                // 双色球期数
+        asset jackpot;                // 资金池总量
+        uint16_t status;              // 此轮双色球游戏的状态，未开奖、正在开奖、已开奖
+        std::vector<uint16_t> hitnum; // 当期开奖号码，红球、蓝球都放在一个数组中，前面6个表示红球，最后一个表示蓝球
 
-                EOSLIB_SERIALIZE( game, (period)(jackpot)(status)(winning_number) )
-            };
+        uint64_t primary_key() const { return draw; }
 
-            //@abi table offerbet i64
-            struct offerbet {
-                uint64_t                    id;             // 主键，使用available_primary_key生成
-                account_name                player;         // 下注的用户帐号
-                uint64_t                    periods;        // The periods of bets
-                asset                       bet;            // 下注金额
-                uint32_t                    buyCnt;         // 购买的彩票数量，单位为张
-                vector<uint16_t>            buyLottos;      // 记录每次下单购买的彩票数字组合，每7个数字表示一张彩票，所有彩票连续记录在此字段
-                time                        buyTime;        // 购买的时间
+        EOSLIB_SERIALIZE(game, (draw)(jackpot)(status)(hitnum))
+    };
 
-                uint64_t        primary_key()const { return id; }
-                account_name    by_player()const { return player; }
-                uint64_t        by_period() const { return periods; }
+    //@abi table offerbets i64
+    struct offerbet
+    {
+        uint64_t id;                     // 主键，使用available_primary_key生成
+        account_name player;             // 下注的用户帐号
+        uint64_t draw;                   // 双色球期数
+        asset bet;                       // 下注金额
+        uint32_t buycnt;                 // 购买的彩票数量，单位为张
+        std::vector<uint16_t> buylottos; // 记录每次下单购买的彩票数字组合，每7个数字表示一张彩票，所有彩票连续记录在此字段
+        time buytime;                    // 购买的时间
 
-                EOSLIB_SERIALIZE( offerbet, (id)(player)(periods)(bet)(buyLottos)(buyTime) )
-            };
-            
-            //@abi table winningrec i64
-            struct winningrec {
-                uint64_t                    id;             // 主键，使用available_primary_key生成
-                account_name                winner;         // 中奖账户
-                uint64_t                    period;         // 中奖期数
-                asset                       win_currency;   // 中奖金额
-                vector<uint16_t>            offer_number;   // 中奖账户购买的彩票号码
-                uint16_t                    prize;          // 表示账户中的是几等奖
+        uint64_t primary_key() const { return id; }
+        account_name by_player() const { return player; }
+        uint64_t by_draw() const { return draw; }
 
-                uint64_t        primary_key()const { return id; }
-                uint64_t        by_period() const { return period; }
+        EOSLIB_SERIALIZE(offerbet, (id)(player)(draw)(bet)(buycnt)(buylottos)(buytime))
+    };
 
-                EOSLIB_SERIALIZE( winningrec, (id)(winner)(period)(win_currency)(offer_number)(prize) )
-            };
+    //@abi table winnings i64
+    struct winning
+    {
+        uint64_t id;                    // 主键，使用available_primary_key生成
+        account_name winner;            // 中奖账户
+        uint64_t draw;                  // 中奖期数
+        asset currency;                 // 中奖金额
+        std::vector<uint16_t> offernum; // 中奖账户购买的彩票号码
+        uint16_t prize;                 // 表示账户中的是几等奖
 
-        private:
+        uint64_t primary_key() const { return id; }
+        uint64_t by_draw() const { return draw; }
 
-            typedef eosio::multi_index<N(game), game> game_index;
+        EOSLIB_SERIALIZE(winning, (id)(winner)(draw)(currency)(offernum)(prize))
+    };
 
-            typedef eosio::multi_index<N(offer_bets), offerbet,
-                indexed_by<N(periods), const_mem_fun<offerbet, uint64_t, &offerbet::by_period > >,
-                indexed_by<N(player), const_mem_fun<offerbet, account_name, &offerbet::by_player> >
+  private:
+    typedef eosio::multi_index<N(games), game> game_index;
+
+    typedef eosio::multi_index<N(offerbets), offerbet,
+                indexed_by<N(draw), const_mem_fun<offerbet, uint64_t, &offerbet::by_draw>>,
+                indexed_by<N(player), const_mem_fun<offerbet, account_name, &offerbet::by_player>>
             > offer_bets_index;
 
-            typedef eosio::multi_index<N(winningrec), winningrec,
-                indexed_by<N(period), const_mem_fun<winningrec, uint64_t, &winningrec::by_period>>
+    typedef eosio::multi_index<N(winnings), winning,
+                indexed_by<N(draw), const_mem_fun<winning, uint64_t, &winning::by_draw>>
             > winning_record_index;
 
-            game_index              games;
-            offer_bets_index        offer_bets;
-            winning_record_index    winning_recs;
+    game_index games;
+    offer_bets_index offerbets;
+    winning_record_index winnings;
 
-        public:
-            trentlottery(account_name self)
-                :eosio::contract(self),
-                games(_self, _self),
-                offer_bets(_self, _self),
-                winning_recs(_self, _self)
-                {}
+  public:
+    trentlottery(account_name self)
+        : eosio::contract(self),
+          games(_self, _self),
+          offerbets(_self, _self),
+          winnings(_self, _self)
+    {
+    }
 
-            void playerbet(uint64_t period, account_name player, const asset& bet, const uint32_t buyCnt, vector<uint16_t> &bills);
-    };
-}
+    void playerbet(uint64_t draw, account_name player, const asset &bet, const uint32_t buycnt, std::vector<uint16_t> bills);
+    void creategame(uint64_t draw, const asset &jackpot, uint16_t status, std::vector<uint16_t> hitnum);
+};
